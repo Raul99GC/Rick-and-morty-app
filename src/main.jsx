fd2039b0fbf4ec62a14b9fab44319924afb9bd69
Raul C <raul.gines1999@gmail.com>
2022-06-13 23:47:16 -0500

    first commit, practicamente terminado, se me olvido subirlo desde un inicio xD

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import './assets/css/normalize.css'
+import RickAndMortyApp from './RickAndMortyApp'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RickAndMortyApp />
+  </React.StrictMode>
+)
